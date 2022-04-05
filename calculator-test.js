70d64b0245dac06d8ed9a33de98e0b13c7de607b
fredeggs <ian.dauphin@gmail.com>
2022-04-05 16:45:21 -0400

    added functional code to calculator.js and test code

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,13 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment({amount: 25000, years: 3, rate: 10})).toBe(806.68);
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  expect(calculateMonthlyPayment({amount: 24563, years: 10, rate: 10})).toBe(324.60);
 });
 
-/// etc
+it('should throw an error if any of the inputs are NaN', function(){
+  expect(() => calculateMonthlyPayment({amount: 'hello', years: 10, rate: 10})).toThrowError();
+})
--- calculator.js
+++ calculator.js
@@ -21,20 +21,41 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let initialValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(initialValues));
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let currentValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentValues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  for(input in values){
+    if(isNaN(values[input])){
+      throw new Error('Invalid Input! Please enter a number');
+    }
+  }
+  let amount = values.amount;
+  let months = values.years * 12;
+  let monthlyRate = values.rate / 1200;
+
+  let monthlyPayment = (amount * monthlyRate) / (1 - (Math.pow((1+monthlyRate), (-1 * months))));
+  let roundedMonthlyPayment = Math.round(monthlyPayment*100)/100;
+
+  return roundedMonthlyPayment;
+  
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  let paymentSpan = document.getElementById("monthly-payment");
+  paymentSpan.innerText = monthly;
 }
